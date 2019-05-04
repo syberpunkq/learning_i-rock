11becd76dd896d77485a5386f4a47c99af698694
syberpunkq <syberpunkq@gmail.com>
2019-05-05 01:14:09 +0300

    Initial commit

---
+++ spec/factories/achievements.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :achievement do
+    sequence(:title) { |n| "Achievement #{n}"}
+    description { "Description" }
+    featured { false }
+    cover_image { "some_file.png" }
+
+    factory :public_achievement do
+      privacy { :public_access }
+    end
+    factory :private_achievement do
+      privacy { :private_access }
+    end
+  end
+end
